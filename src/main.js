ce8629f3b10219af771fb1bd49088af2c8a99299
gaiting.feng <gaiting.feng@kaiostech.com>
2019-11-20 11:19:22 +0800

    Bug when run serve

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import store from './store/index'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/css/loading.css'
-import './assets/css/boading-btn.css'
+import './assets/css/loading-btn.css'
 
 
 Vue.config.productionTip = false
